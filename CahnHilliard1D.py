affce395ae7c07ddcdd63adce3b89fd6a384785b
A. Mulard <am2548@tw311.eng.cam.ac.uk>
2017-04-07 15:21:47 +0100

    Solve rank problem

--- CahnHilliard1D.py
+++ CahnHilliard1D.py
@@ -87,7 +87,8 @@ pressureRelaxation = 0.8
 velocityRelaxation = 0.5
 pressureCorrection = CellVariable(mesh=mesh)
 Velocity = FaceVariable(mesh=mesh)
-velocityEq = (ImplicitSourceTerm(coeff=beta) == pressure.grad)
+from fipy.variables.faceGradVariable import _FaceGradVariable
+velocityEq = (ImplicitSourceTerm(coeff=beta) == pressure.grad[0.,])
 ap = CellVariable(mesh=mesh, value=1.)
 
 from fipy.variables.faceGradVariable import _FaceGradVariable
@@ -95,36 +96,78 @@ volume = CellVariable(mesh=mesh, value=mesh.cellVolumes, name='Volume')
 contrvolume=volume.arithmeticFaceValue
 
 coeff = 1./ ap.arithmeticFaceValue*mesh._faceAreas * mesh._cellDistances
-pressureCorrectionEq = DiffusionTerm(coeff=coeff) - velocity.divergence
+pressureCorrectionEq = DiffusionTerm(coeff=coeff) - Velocity.divergence
 X = mesh.faceCenters
 pressureCorrection.constrain(0., mesh.facesLeft)
 
-def solveStokes(phi):
+"""def solveStokes(phi):
 	for sweep in range(sweeps):
+		#Calculations of the fields at the new time using the latest solution un and pn as starting estimates for un+1 and pn+1
+		#Assemble and solve the linearized algebraiec equation systems for the velocity components to obtain ustarred
 		velocityEq.cacheMatrix()
 		xres = velocityEq.sweep(var=velocity, underRelaxation=velocityRelaxation)
 		xmat = velocityEq.matrix
+		#update matrix
 		ap[:] = - xmat.takeDiagonal()
+		#Correction due to colocated grid
 		presgrad = pressure.grad
 		facepresgrad = _FaceGradVariable(pressure)
 		Velocity=velocity.arithmeticFaceValue + contrvolume / ap.arithmeticFaceValue * (presgrad.arithmeticFaceValue-facepresgrad)
+		#Assemble and solve the pressure correction equation to obtain p'
 		pressureCorrectionEq.cacheRHSvector()
+		#correct velocity and pressure to obtain um, which satisfies the continuity and new pressure pm
 		pressure.setValue(pressure + pressureRelaxation * pressureCorrection)
 		velocity.setValue(velocity - pressureCorrection.grad / ap * mesh.cellVolumes)
-		viewer2.plot()
+		return
+		#return to start and begin again with um and pm as improved estimates until all corrections are negligibly small
 		
-
+"""
 #-----------------------------------------------------------------------
 #--------------------------Time Resolution------------------------------
 #-----------------------------------------------------------------------
 
 dt = numerix.exp(-5)
 time = 0.
-
+"""
 while time < 1000:
 	time +=dt
 	eq.solve(phi, dt=dt)
-	solveStokes(phi)
-	viewer.plot()
+	for sweep in range(sweeps):
+		#Calculations of the fields at the new time using the latest solution un and pn as starting estimates for un+1 and pn+1
+		#Assemble and solve the linearized algebraiec equation systems for the velocity components to obtain ustarred
+		velocityEq.cacheMatrix()
+		xres = velocityEq.sweep(var=velocity, underRelaxation=velocityRelaxation)
+		xmat = velocityEq.matrix
+		#update matrix
+		ap[:] = - xmat.takeDiagonal()
+		#Correction due to colocated grid
+		presgrad = pressure.grad
+		facepresgrad = _FaceGradVariable(pressure)
+		Velocity=velocity.arithmeticFaceValue + contrvolume / ap.arithmeticFaceValue * (presgrad.arithmeticFaceValue-facepresgrad)
+		#Assemble and solve the pressure correction equation to obtain p'
+		pressureCorrectionEq.cacheRHSvector()
+		#correct velocity and pressure to obtain um, which satisfies the continuity and new pressure pm
+		pressure.setValue(pressure + pressureRelaxation * pressureCorrection)
+		velocity.setValue(velocity - pressureCorrection.grad / ap * mesh.cellVolumes)
+	viewer2.plot()
+"""
 
+eq.solve(phi, dt=dt)
+
+#Calculations of the fields at the new time using the latest solution un and pn as starting estimates for un+1 and pn+1
+#Assemble and solve the linearized algebraiec equation systems for the velocity components to obtain ustarred
+velocityEq.cacheMatrix()
+xres = velocityEq.sweep(var=velocity, underRelaxation=velocityRelaxation)
+xmat = velocityEq.matrix
+#update matrix
+ap[:] = - xmat.takeDiagonal()
+#Correction due to colocated grid
+presgrad = pressure.grad
+facepresgrad = _FaceGradVariable(pressure)
+Velocity=velocity.arithmeticFaceValue + contrvolume / ap.arithmeticFaceValue * (presgrad.arithmeticFaceValue-facepresgrad)
+#Assemble and solve the pressure correction equation to obtain p'
+pressureCorrectionEq.cacheRHSvector()
+#correct velocity and pressure to obtain um, which satisfies the continuity and new pressure pm
+pressure.setValue(pressure + pressureRelaxation * pressureCorrection)
+velocity.setValue(velocity - pressureCorrection.grad / ap * mesh.cellVolumes)
 
